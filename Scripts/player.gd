cfc7cbab2614737b8a957dd5c65e6fc7abd45e8c
Casey Bracken <bracc004@gmail.com>
2026-09-12 16:53:18 +0100

    Dialogic testing

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -4,26 +4,18 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-signal advance_dialogue
-
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	
+	var direction: float = 0.0
+	if Dialogic.current_timeline == null:
+		# Handle jump.
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		direction = Input.get_axis("ui_left", "ui_right")
+	velocity.x = direction * SPEED
 
 	move_and_slide()
-
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		print("input received to advance dialogue")
-		DialogueSignal.emit_advance_dialogue()
