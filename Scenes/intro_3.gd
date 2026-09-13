1bd92ffaeecdb0b93eea628c7ca0881a5f85a9e2
RobertFOConnor <robertoconnor94@gmail.com>
2026-09-13 13:02:03 +0100

    intro scenes

---
+++ Scenes/intro_3.gd
@@ -0,0 +1,7 @@
+extends Node2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func play():
+	animation_player.play("new_animation")
